f24f9218d437df647de5cc17f72f7e9cd55e4b76
apeTrackunit <ape@trackunit.com>
2023-04-27 23:21:49 +0200

    Init of swr and axios

--- libs/home/src/lib/home.tsx
+++ libs/home/src/lib/home.tsx
@@ -1,12 +1,14 @@
 import { TestComponent } from '@lizards-inc-fe/shared-components';
+import { useGetRequest } from '@lizards-inc-fe/fetcher';
 
 export function Home() {
+  const { data } = useGetRequest<object>({ url: '/pokemon' });
+
   return (
     <div>
       <h1>Welcome to Home!</h1>
+      {JSON.stringify(data)}
       <TestComponent />
     </div>
   );
 }
-
-export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,18 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
+import { SWRConfig } from 'swr';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
-    <App />
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json()),
+        revalidateOnFocus: false,
+      }}
+    >
+      <App />
+    </SWRConfig>
   </StrictMode>
 );
