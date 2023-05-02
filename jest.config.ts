db5f340edef88b68885e206022880e2cc93b95a1
Enbi81 <wotng2002@gmail.com>
2023-05-02 23:44:48 +0200

    setting up coverage test - fully working

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,14 @@ import { getJestProjects } from '@nrwl/jest';
 
 export default {
   projects: getJestProjects(),
+
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+
   // Enable coverage collection
   collectCoverage: true,
   // Define coverage reporters
