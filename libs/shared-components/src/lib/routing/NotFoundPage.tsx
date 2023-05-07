49a1198c27303313a81e7f283a37b9e027c7c2de
Adam Petricek <78472079+apeTrackunit@users.noreply.github.com>
2023-05-07 18:55:58 +0300

    Error page redesign (#8)
    * done
    
    * Renaming
    
    * Deleted unused file

--- libs/shared-components/src/lib/routing/NotFoundPage.tsx
+++ libs/shared-components/src/lib/routing/NotFoundPage.tsx
@@ -1,8 +1,11 @@
+import ErrorLogo from '../error-logo.png';
+
 export const NotFoundPage = () => {
   return (
-    <>
-      <h1>Error 404 - Page not found</h1>
+    <div className={'w-full h-full flex flex-col gap-4 items-center justify-center'}>
+      <img src={ErrorLogo} alt={'error-logo'} className={'h-64'} />
+      <div className={'text-2xl'}>Error 404 - Page not found</div>
       <p>Use side menu to navigate in the app</p>
-    </>
+    </div>
   );
 };
