77a946b8198939539859102ddc05e6c45cefadc6
apeTrackunit <ape@trackunit.com>
2023-05-08 19:35:40 +0200

    First changes of design

--- libs/home/src/lib/Home.tsx
+++ libs/home/src/lib/Home.tsx
@@ -1,6 +1,8 @@
 import { useGetRequest } from '@lizards-inc-fe/fetcher';
-import { Card, Col, Divider, Row, Statistic } from 'antd';
-import colors from 'tailwindcss/colors';
+import { Card, Divider, Statistic } from 'antd';
+import TemperatureIcon from './assets/temperature-icon.png';
+import Co2Icon from './assets/co2-icon.png.png';
+import HumidityIcon from './assets/humidity-icon.png-icon.png';
 
 interface IMeasurement {
   id: string;
@@ -26,13 +28,13 @@ export function Home() {
           <>
             <span style={{ paddingRight: '4px' }}>Measured at {data?.time.substring(0, 8) + ' on ' + data?.date}</span>
             <div className="flex gap-4 justify-around sm:flex-row flex-col">
-              <Card className={'bg-red-300'}>
+              <Card className={'bg-red-100 w-60'}>
                 <Statistic title="Temperature" value={`${data?.temperature} °C`} />
               </Card>
-              <Card className={'bg-blue-300'}>
+              <Card className={'bg-blue-100 w-60'}>
                 <Statistic title="Humidity" value={`${data?.humidity} %`} />
               </Card>
-              <Card className={'bg-green-300'}>
+              <Card className={'bg-green-100 w-60'}>
                 <Statistic title="CO2" value={`${data?.co2} %`} />
               </Card>
             </div>
