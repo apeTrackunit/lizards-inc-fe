8e6d5c3f855134c7fcaac08fac2ab6d80ea4368d
apeTrackunit <ape@trackunit.com>
2023-05-08 19:58:33 +0200

    designed page

--- libs/home/src/lib/Home.tsx
+++ libs/home/src/lib/Home.tsx
@@ -1,8 +1,8 @@
 import { useGetRequest } from '@lizards-inc-fe/fetcher';
 import { Card, Divider, Statistic } from 'antd';
 import TemperatureIcon from './assets/temperature-icon.png';
-import Co2Icon from './assets/co2-icon.png.png';
-import HumidityIcon from './assets/humidity-icon.png-icon.png';
+import Co2Icon from './assets/co2-icon.png';
+import HumidityIcon from './assets/humidity-icon.png';
 
 interface IMeasurement {
   id: string;
@@ -21,24 +21,41 @@ export function Home() {
       <h1 className={'text-2xl'}>Home</h1>
       <Divider />
       <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <h4>Cage status</h4>
+        <h1 className={'font-medium text-lg'}>Current status</h1>
         {isLoading && <div>Loading...</div>}
 
         {!isLoading && (
-          <>
-            <span style={{ paddingRight: '4px' }}>Measured at {data?.time.substring(0, 8) + ' on ' + data?.date}</span>
+          <div className={'flex flex-col gap-4'}>
+            <span className={'grid justify-items-end text-gray-500'}>
+              Measured at {data?.time.substring(0, 8) + ' on ' + data?.date}
+            </span>
             <div className="flex gap-4 justify-around sm:flex-row flex-col">
-              <Card className={'bg-red-100 w-60'}>
-                <Statistic title="Temperature" value={`${data?.temperature} °C`} />
+              <Card className={'bg-red-100 w-52 h-fit'}>
+                <Statistic
+                  title="Temperature"
+                  value={`${data?.temperature} °C`}
+                  prefix={<img src={TemperatureIcon} alt={'temperature-logo'} className={'h-8 mr-2'} />}
+                  valueStyle={{ alignItems: 'center', display: 'flex' }}
+                />
               </Card>
-              <Card className={'bg-blue-100 w-60'}>
-                <Statistic title="Humidity" value={`${data?.humidity} %`} />
+              <Card className={'bg-blue-100 w-52 h-fit'}>
+                <Statistic
+                  title="Humidity"
+                  value={`${data?.humidity} %`}
+                  prefix={<img src={HumidityIcon} alt={'humidity-logo'} className={'h-8 mr-2'} />}
+                  valueStyle={{ alignItems: 'center', display: 'flex' }}
+                />
               </Card>
-              <Card className={'bg-green-100 w-60'}>
-                <Statistic title="CO2" value={`${data?.co2} %`} />
+              <Card className={'bg-green-100 w-52 h-fit'}>
+                <Statistic
+                  title="CO2"
+                  value={`${data?.co2} %`}
+                  prefix={<img src={Co2Icon} alt={'co2-logo'} className={'h-8 mr-2'} />}
+                  valueStyle={{ alignItems: 'center', display: 'flex' }}
+                />
               </Card>
             </div>
-          </>
+          </div>
         )}
       </div>
     </div>
