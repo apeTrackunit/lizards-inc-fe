9f6a45469bbdb63a040378a79004aec4998aad43
Gabriel <315204@via.dk>
2023-05-15 14:33:59 +0200

    Added the notification component, changed the font and color of other stuff

---
+++ libs/limits-and-boundaries/src/lib/components/Notification.tsx
@@ -0,0 +1,28 @@
+import React, { useMemo } from 'react';
+import { NotificationPlacement } from 'antd/es/notification/interface';
+import { Button, notification } from 'antd';
+
+function Notification() {
+  type NotificationType = 'success' | 'info' | 'warning' | 'error';
+  const Context = React.createContext({ text: 'Default' });
+  const [api, contextHolder] = notification.useNotification();
+  const openNotification = (placement: NotificationPlacement, type: NotificationType) => {
+    api[type]({
+      message: <Context.Consumer>{({ text }) => text}</Context.Consumer>,
+      placement,
+    });
+  };
+
+  const contextValue = useMemo(() => ({ text: 'Changes successfully saved!' }), []);
+
+  return (
+    <Context.Provider value={contextValue}>
+      {contextHolder}
+      <Button className="rounded-none bg-[#91caff] px-6" onClick={() => openNotification('bottomRight', 'success')}>
+        <div className="mx-6 my-1 text-xl">Save</div>
+      </Button>
+    </Context.Provider>
+  );
+}
+
+export default Notification;
--- libs/limits-and-boundaries/src/lib/limits-and-boundaries.tsx
+++ libs/limits-and-boundaries/src/lib/limits-and-boundaries.tsx
@@ -1,138 +1,124 @@
-import React from 'react';
-import {Divider, InputNumber} from 'antd';
+import React, { useMemo } from 'react';
+import { Button, Divider, Space, notification, Tooltip } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Dropdown, Space } from 'antd';
 import { Slider } from 'antd';
 import type { SliderMarks } from 'antd/es/slider';
+import Notification from './components/Notification';
+import { NotificationPlacement } from 'antd/es/notification/interface';
 
 /* eslint-disable-next-line */
 export interface LimitsAndBoundariesProps {}
 
 const style: React.CSSProperties = {
-    marginLeft: 70,
-    marginRight: 70,
-    marginTop: 20
+  marginLeft: 70,
+  marginRight: 70,
+  marginTop: 20,
 };
 
 const marksTemp: SliderMarks = {
-    0: {
-        label: <strong>0°C</strong>,
-    },
-    50: {
-        label: <strong>50°C</strong>,
-    },
+  0: {
+    label: '0°C',
+  },
+  50: {
+    label: '50°C',
+  },
 };
 
 const marksHum: SliderMarks = {
-    0: {
-        label: <strong>0%</strong>,
-    },
-    100: {
-        label: <strong>100%</strong>,
-    },
+  0: {
+    label: '0%',
+  },
+  100: {
+    label: '100%',
+  },
 };
 
 const marksCO2: SliderMarks = {
-    0: {
-        label: <strong>0 ppm</strong>,
-    },
-    10000: {
-        label: <strong> 10000 ppm</strong>,
-    },
+  0: {
+    label: '0 ppm',
+  },
+  10000: {
+    label: '10000 ppm',
+  },
 };
-
-const info: MenuProps['items'] = [
-    {
-        label: (
-            <p>
-                Used for selecting the temperature at which the cooling and heating should start working.
-            </p>
-        ),
-        key: '0',
-    },
-];
-
-const info1: MenuProps['items'] = [
-    {
-        label: (
-            <p>
-                Used for selecting the temperature, humidity or levels of CO at which
-                you want to receive notifications.
-            </p>
-        ),
-        key: '1',
-    }
-];
 
 export function LimitsAndBoundaries(props: LimitsAndBoundariesProps) {
   return (
     <div>
-        <div className="flex flex-row-reverse">
-            <button className="rounded-none bg-sky-500 hover:bg-cyan-600">
-                <div className="px-5 py-1">
-                    Save
+      <div className="flex flex-row-reverse">
+        <Notification />
+      </div>
+      <div>
+        <div className="flex">
+          <p className="text-4xl">Limits</p>
+          <div className="pl-2">
+            <Tooltip
+              title={
+                <div className="text-black">
+                  Used for selecting the temperature at which the cooling and heating should start working.
                 </div>
-            </button>
+              }
+              placement="right"
+              color="white"
+            >
+              <InfoCircleOutlined />
+            </Tooltip>
+          </div>
         </div>
+        <Divider />
         <div>
-            <div className="flex">
-                <p className = "text-4xl">Limits</p>
-                <div>
-                    <Dropdown menu={ { items:info } }>
-                        <Space>
-                            <InfoCircleOutlined />
-                        </Space>
-                    </Dropdown>
-                </div>
-            </div>
-            <Divider/>
-            <div >
-                <p className = "text-2xl text-zinc-500">Temperature</p>
-                <div style={style}>
-                    <Slider range marks={marksTemp} defaultValue={[0, 38]} max={50}/>
-                </div>
-            </div>
+          <p className="text-2xl text-zinc-500">Temperature</p>
+          <div style={style}>
+            <Slider range marks={marksTemp} defaultValue={[0, 38]} max={50} />
+          </div>
         </div>
-        <div>
-            <div className = "flex pt-7">
-                <p className = "text-4xl">Boundaries</p>
-                <div>
-                    <Dropdown menu={{ items:info1 }}>
-                        <Space>
-                            <InfoCircleOutlined />
-                        </Space>
-                    </Dropdown>
+      </div>
+      <div>
+        <div className="flex pt-7">
+          <p className="text-4xl">Boundaries</p>
+          <div className="pl-2">
+            <Tooltip
+              title={
+                <div className="text-black">
+                  Used for selecting the temperature, humidity or levels of CO at which you want to receive
+                  notifications.
                 </div>
-            </div>
-            <Divider />
-            <div className = "flex flex-row">
-                <div className = "basis-1/3">
-                    <p className = "text-2xl text-zinc-500">Temperature</p>
-                    <div>
-                        <div className = "w-5/6">
-                            <Slider range marks={marksTemp} defaultValue={[0, 38]} max={50}/>
-                        </div>
-                    </div>
-                </div>
-                <div className = "basis-1/3">
-                    <p className = "text-2xl text-zinc-500">Humidity</p>
-                    <div>
-                        <div className = "w-5/6">
-                            <Slider range marks={marksHum} defaultValue={[0, 38]} max={100}/>
-                        </div>
-                    </div>
-                </div>
-                <div className = "basis-1/3">
-                    <p className = "text-2xl text-zinc-500">C02</p>
-                    <div>
-                        <div className = "w-5/6">
-                            <Slider range marks={marksCO2} defaultValue={[0, 38]} max={10000}/>
-                        </div>
-                    </div>
-                </div>
-            </div>
+              }
+              placement="right"
+              color="white"
+            >
+              <InfoCircleOutlined />
+            </Tooltip>
+          </div>
         </div>
+        <Divider />
+        <div className="flex flex-row">
+          <div className="basis-1/3">
+            <p className="text-2xl text-zinc-500">Temperature</p>
+            <div>
+              <div className="w-5/6">
+                <Slider range marks={marksTemp} defaultValue={[0, 38]} max={50} />
+              </div>
+            </div>
+          </div>
+          <div className="basis-1/3">
+            <p className="text-2xl text-zinc-500">Humidity</p>
+            <div>
+              <div className="w-5/6">
+                <Slider range marks={marksHum} defaultValue={[0, 38]} max={100} />
+              </div>
+            </div>
+          </div>
+          <div className="basis-1/3">
+            <p className="text-2xl text-zinc-500">C02</p>
+            <div>
+              <div className="w-5/6">
+                <Slider range marks={marksCO2} defaultValue={[0, 38]} max={10000} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
