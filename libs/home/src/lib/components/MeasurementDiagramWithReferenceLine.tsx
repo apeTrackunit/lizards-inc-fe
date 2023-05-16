8b1a35d02909a9367cbe569eee35489bd7248bf0
Enbi81 <wotng2002@gmail.com>
2023-05-16 22:02:01 +0200

    creating measurement diagram

---
+++ libs/home/src/lib/components/MeasurementDiagramWithReferenceLine.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine } from 'recharts';
+import { Skeleton } from 'antd';
+
+const data = [
+  {
+    name: '2022.05.12',
+    data: 14,
+  },
+  {
+    name: '2022.05.13',
+    data: 17,
+  },
+];
+
+export interface DiagramLine {
+  y: number;
+  color: string;
+  label: string;
+}
+
+export interface DiagramData {
+  name: string;
+  data: number;
+}
+
+interface MeasurementDiagramWithReferenceLineProps {
+  isLoading: boolean;
+  height: number;
+  width: number;
+  lines: DiagramLine[];
+  data: DiagramData[] | undefined;
+  dataColor: string;
+  dataName: string;
+}
+
+export const MeasurementDiagramWithReferenceLine = ({
+  isLoading,
+  height,
+  width,
+  lines,
+  data,
+  dataColor,
+  dataName,
+}: MeasurementDiagramWithReferenceLineProps) => {
+  return isLoading ? (
+    <Skeleton.Avatar active={true} size={Math.min(width, height)} shape={'square'} />
+  ) : (
+    <LineChart width={width} height={height} data={data}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend align={'right'} />
+      {lines.map(line => (
+        <ReferenceLine y={line.y} label={line.label} stroke={line.color} />
+      ))}
+      <Line type="monotone" dataKey="data" stroke={dataColor} name={dataName} />
+    </LineChart>
+  );
+};
