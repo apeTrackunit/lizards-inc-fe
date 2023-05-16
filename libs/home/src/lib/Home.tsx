e087bf72516134dc09211dcd02822810ac40dc24
Enbi81 <wotng2002@gmail.com>
2023-05-16 22:36:18 +0200

    adding main measurement containers

--- libs/home/src/lib/Home.tsx
+++ libs/home/src/lib/Home.tsx
@@ -5,17 +5,41 @@ import Co2Icon from './assets/co2-icon.png';
 import HumidityIcon from './assets/humidity-icon.png';
 import { MeasurementCard } from './components/MeasurementCard';
 import moment from 'moment';
+import { IMeasurement } from './IMeasurement';
+import { IBoundary } from './IBoundary';
+import { MeasurementContainer } from './components/MeasurementContainer';
 
-interface IMeasurement {
-  id: string;
-  temperature: number;
-  humidity: number;
-  co2: number;
-  dateTime: Date;
-}
+const getLatestMeasurement = () => {
+  const response = useGetRequest<IMeasurement>({ url: '/Measurements/latest' });
+
+  return {
+    latestMeasurement: response.data,
+    isLatestMeasurementLoading: response.isLoading,
+  };
+};
+
+const getBoundaries = () => {
+  const response = useGetRequest<IBoundary>({ url: '/Terrarium/boundaries' });
+
+  return {
+    boundaries: response.data,
+    isBoundariesLoading: response.isLoading,
+  };
+};
+
+const getMeasurementsRange = () => {
+  const response = useGetRequest<IMeasurement[]>({ url: '/Measurements/all' });
+
+  return {
+    measurementRange: response.data,
+    isMeasurementRangeLoading: response.data,
+  };
+};
 
 export const Home = () => {
-  const { data, isLoading } = useGetRequest<IMeasurement>({ url: '/Measurements/latest' });
+  const { latestMeasurement, isLatestMeasurementLoading } = getLatestMeasurement();
+  const { boundaries } = getBoundaries();
+  const { measurementRange } = getMeasurementsRange();
 
   return (
     <div>
@@ -25,33 +49,89 @@ export const Home = () => {
         <h1 className={'font-medium text-lg'}>Current status</h1>
         <div className={'flex flex-col gap-4'}>
           <span className={'grid justify-items-end text-gray-500'}>
-            {isLoading ? (
+            {isLatestMeasurementLoading ? (
               <Skeleton active={true} paragraph={false} className={'max-w-md'} />
             ) : (
-              <span>Measured at {moment(data?.dateTime).format('h:mm:ss DD.MM.YYYY')}</span>
+              <span>Measured at {moment(latestMeasurement?.dateTime).format('hh:mm:ss DD.MM.YYYY')}</span>
             )}
           </span>
-          <div className="flex gap-4 lg:justify-around items-center lg:flex-row flex-col">
-            <MeasurementCard
-              isLoading={isLoading}
+          <div className="flex gap-4 xl:justify-around items-center xl:flex-row flex-col flex-wrap">
+            <MeasurementContainer
               title={'Temperature'}
-              icon={TemperatureIcon}
-              value={`${data?.temperature} °C`}
-              cardClassName={'bg-red-100 w-full lg:w-60 h-fit'}
+              cardConfig={{
+                cardColor: 'bg-red-100',
+                icon: TemperatureIcon,
+              }}
+              diagramConfig={{
+                hexColor: '#e30000',
+              }}
+              userData={{
+                boundaries:
+                  boundaries == undefined
+                    ? undefined
+                    : { min: boundaries.temperatureBoundaryMin, max: boundaries.temperatureBoundaryMax },
+                historyMeasurements:
+                  measurementRange == undefined
+                    ? undefined
+                    : measurementRange.map(measurement => ({
+                        name: moment(measurement.dateTime).format('yyyy.MM.dd'),
+                        data: measurement.temperature,
+                      })),
+                measurementData: latestMeasurement?.temperature,
+                measurementDisplayData: `${latestMeasurement?.temperature} °C`,
+              }}
             />
-            <MeasurementCard
-              isLoading={isLoading}
+            <Divider className={'xl:hidden'} />
+            <MeasurementContainer
               title={'Humidity'}
-              icon={HumidityIcon}
-              value={`${data?.humidity} %`}
-              cardClassName={'bg-blue-100 w-full lg:w-60 h-fit'}
+              cardConfig={{
+                cardColor: 'bg-blue-100',
+                icon: HumidityIcon,
+              }}
+              diagramConfig={{
+                hexColor: '#00f',
+              }}
+              userData={{
+                boundaries:
+                  boundaries == undefined
+                    ? undefined
+                    : { min: boundaries.humidityBoundaryMin, max: boundaries.humidityBoundaryMax },
+                historyMeasurements:
+                  measurementRange == undefined
+                    ? undefined
+                    : measurementRange.map(measurement => ({
+                        name: moment(measurement.dateTime).format('yyyy.MM.dd'),
+                        data: measurement.humidity,
+                      })),
+                measurementData: latestMeasurement?.humidity,
+                measurementDisplayData: `${latestMeasurement?.humidity} %`,
+              }}
             />
-            <MeasurementCard
-              isLoading={isLoading}
+            <Divider className={'xl:hidden'} />
+            <MeasurementContainer
               title={'CO2'}
-              icon={Co2Icon}
-              value={`${data?.co2} ppm`}
-              cardClassName={'bg-green-100 w-full lg:w-60 h-fit'}
+              cardConfig={{
+                cardColor: 'bg-green-100',
+                icon: Co2Icon,
+              }}
+              diagramConfig={{
+                hexColor: '#00b700',
+              }}
+              userData={{
+                boundaries:
+                  boundaries == undefined
+                    ? undefined
+                    : { min: boundaries.cO2BoundaryMin, max: boundaries.cO2BoundaryMax },
+                historyMeasurements:
+                  measurementRange == undefined
+                    ? undefined
+                    : measurementRange.map(measurement => ({
+                        name: moment(measurement.dateTime).format('yyyy.MM.dd'),
+                        data: measurement.co2,
+                      })),
+                measurementData: latestMeasurement?.co2,
+                measurementDisplayData: `${latestMeasurement?.co2} ppm`,
+              }}
             />
           </div>
         </div>
