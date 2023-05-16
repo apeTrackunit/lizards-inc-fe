8361b60486cf47962c0017ed88ccb6fc76c7ecc8
Adam Petricek <78472079+apeTrackunit@users.noreply.github.com>
2023-05-16 14:45:53 +0200

    Post and put hook (#19)

---
+++ libs/fetcher/src/lib/useMutateRequest.ts
@@ -0,0 +1,57 @@
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+import useSWRMutation, { SWRMutationConfiguration, SWRMutationResponse } from 'swr/mutation';
+
+export type Request = AxiosRequestConfig | null;
+
+interface Return<Data, Error>
+  extends Pick<
+    SWRMutationResponse<AxiosResponse<Data>, AxiosError<Error>>,
+    'error' | 'isMutating' | 'trigger' | 'reset'
+  > {
+  data: Data | undefined;
+  response: AxiosResponse<Data> | undefined;
+}
+
+export interface Config<Data = unknown, Error = unknown>
+  extends Omit<SWRMutationConfiguration<AxiosResponse<Data>, AxiosError<Error>>, 'fallbackData'> {
+  fallbackData?: Data;
+}
+
+export function useMutateRequest<Data = unknown, Error = unknown>(
+  request: Request
+  //{ fallbackData, ...config }: Config<Data, Error> = {}
+): Return<Data, Error> {
+  const {
+    data: response,
+    error,
+    isMutating,
+    trigger,
+    reset,
+  } = useSWRMutation<AxiosResponse<Data>, AxiosError<Error>>(
+    request,
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    () => axios.request<Data>(request!)
+    /*{
+      ...config,
+      fallbackData:
+        fallbackData &&
+        ({
+          status: 200,
+          statusText: 'InitialData',
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          config: request!,
+          headers: {},
+          data: fallbackData,
+        } as AxiosResponse<Data>),
+    }*/
+  );
+
+  return {
+    data: response && response.data,
+    response,
+    error,
+    isMutating,
+    reset,
+    trigger,
+  };
+}
