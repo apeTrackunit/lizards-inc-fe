7fc10ef8477c3cdfee9895d21a49512c9f0adeb5
Enbi81 <wotng2002@gmail.com>
2023-05-18 11:23:56 +0200

    adding mock handler for authentication

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,6 +1,7 @@
 import MeasurementHandlers from './handlers/measurement.handler';
 import BoundariesHandler from './handlers/boundaries.handler';
+import AuthenticationHandler from './handlers/authentication.handler';
 
-const handlers = [...MeasurementHandlers, ...BoundariesHandler];
+const handlers = [...MeasurementHandlers, ...BoundariesHandler, ...AuthenticationHandler];
 
 export default handlers;
