a403098029a544699d105d61a68c22f1d9c41b5b
apeTrackunit <ape@trackunit.com>
2023-05-18 10:14:45 +0200

    Fixed tests

--- libs/login/src/lib/Login.spec.tsx
+++ libs/login/src/lib/Login.spec.tsx
@@ -1,16 +1,29 @@
 import { render } from '@testing-library/react';
 import Login from './Login';
+import { AuthProvider } from '@lizards-inc-fe/login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 describe('Login', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Login />);
     expect(baseElement).toBeTruthy();
   });
-});
 
-describe('Card', () => {
   it('should render the card and components successfully', () => {
-    const { baseElement } = render(<Login />);
+    const { baseElement } = render(
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="*"
+            element={
+              <AuthProvider>
+                <Login />
+              </AuthProvider>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    );
     const loginCard = baseElement.id.localeCompare('login-card');
     const loginTitle = baseElement.querySelector('.ant-typography');
     const loginIcon = baseElement.id.localeCompare('user-icon');
