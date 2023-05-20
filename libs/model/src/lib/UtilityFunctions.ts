00245ee6854ded40031ef4ed39c1064e236fa1d3
Enbi81 <wotng2002@gmail.com>
2023-05-20 19:28:17 +0200

    adding documentation for the filterData

--- libs/model/src/lib/UtilityFunctions.ts
+++ libs/model/src/lib/UtilityFunctions.ts
@@ -1,3 +1,12 @@
+/**
+ Filters an array of data and returns a subset of the data based on the specified number of items to return.
+ @template T - The type of data elements in the array.
+ @param data - The array of data to filter.
+ @param numberOfDataToReturn - The number of data items to return.
+ @returns An array containing a subset of the data elements based on the specified number of items to return.
+ @throws Error if numberOfDataToReturn is less than 1.
+ @throws Returns undefined if the input data is undefined.
+ */
 export function filterData<T>(data: T[] | undefined, numberOfDataToReturn: number) {
   if (data == undefined) return undefined;
 
